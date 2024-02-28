c2093d987a4d134f7ef2f127310ce823194b9cd8
josef.navratil <josef.navratil@VT3-08>
2024-02-28 09:08:33 +0100

    skoro done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace YouTube
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace YouTube{
+    public partial class MainWindow : Window{
+        public MainWindow(){
             InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+
+            if (play.Content.ToString() == "Play")
+            {
+                video1.Play();
+                play.Content = "Pause";
+            }
+            else
+            {
+                video1.Pause();
+                play.Content = "Play";
+            }
         }
     }
 }
