50e79428d3700a0c7af0bd5bef4d38d479cc0e2b
Josef Navrátil <josef.navratil@educhem.cz>
2024-04-03 07:42:45 +0200

    hotovo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Win32;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace YouTube{
     public partial class MainWindow : Window{
@@ -8,21 +10,43 @@ namespace YouTube{
             
         }
 
+        int check = 0;
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
+            MainWin.Background = Brushes.Black;
+            MainWin.Foreground = Brushes.White;
+            butn1.Visibility = Visibility.Hidden;
+            butn2.Visibility = Visibility.Visible;
+        }
 
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            MainWin.Background = Brushes.White;
+            MainWin.Foreground = Brushes.Black;
+            butn1.Visibility = Visibility.Visible;
+            butn2.Visibility = Visibility.Hidden;
+        }
 
-            if (play.Content.ToString() == "Play")
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (check == 0)
             {
-                video1.Play();
-                play.Content = "Pause";
+                video1.LoadedBehavior = MediaState.Pause;
+                video2.LoadedBehavior = MediaState.Pause;
+                video3.LoadedBehavior = MediaState.Pause;
+                video4.LoadedBehavior = MediaState.Pause;
+                check = 1;
             }
-            else
+            else if(check == 1)
             {
-                video1.Pause();
-                play.Content = "Play";
+                video1.LoadedBehavior = MediaState.Play;
+                video2.LoadedBehavior = MediaState.Play;
+                video3.LoadedBehavior = MediaState.Play;
+                video4.LoadedBehavior = MediaState.Play;
+                check = 0;
             }
+
+            
         }
     }
 }
